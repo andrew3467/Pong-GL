b8a1ccbec05e577bede9f75c70419a05c3671710
andrew3467 <apgraser2@gmail.com>
2024-11-04 17:59:17 -0500

    Update main.cpp

--- src/Pong-GL/main.cpp
+++ src/Pong-GL/main.cpp
@@ -1,5 +1,13 @@
-#include "Tungsten/Core/Logger.h"
+
+#include "Tungsten.h"
 
 int main() {
+    Tungsten::Logger::Init();
+
+
+
+    TUNGSTEN_ERROR("Dumbass");
+    TUNGSTEN_ASSERT(false, "ERROR: FALSE IS NOT TRUE");
+
     return 0;
 }
