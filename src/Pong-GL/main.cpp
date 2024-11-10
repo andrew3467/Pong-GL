f14c150043d3e192373b505b4349ba6e51821f99
andrew3467 <apgraser2@gmail.com>
2024-11-09 20:33:49 -0500

    Use Renderer

--- src/Pong-GL/main.cpp
+++ src/Pong-GL/main.cpp
@@ -1,13 +1,17 @@
 
 #include "Tungsten.h"
-#include "Tungsten/Core/Window.h"
+
 
 int main() {
     Tungsten::Logger::Init();
 
     Tungsten::Window window({1280, 720, "Simple Pong"});
 
+    Tungsten::Renderer::Init();
+
     while(!window.ShouldClose()) {
+        Tungsten::Renderer::SetClearColor(0.5f, 0.3f, 1.0f);
+        Tungsten::Renderer::Clear();
 
         window.Update();
     }
